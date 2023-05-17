27845e3cc09beb9af48732fe534a93cdef12353d
PepeCharly01 <jc.espinosa2@gmail.com>
2023-05-17 13:35:12 -0600

    estilos

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -6,7 +6,10 @@ function TodoCounter() {
     const { totalTodos, completedTodos } = React.useContext(TodoContext);
 
     return (
-        <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs</h2>
+        <h2 className="TodoCounter">
+            Has completado <span>{completedTodos}</span>
+            de <span>{totalTodos}</span> TODOs
+        </h2>
     );
 }
 
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -4,10 +4,8 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span
-        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={props.onComplete}
-      >
+      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={props.onComplete}>
         √
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
@@ -15,8 +13,7 @@ function TodoItem(props) {
       </p>
       <span
         className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >
+        onClick={props.onDelete}>
         X
       </span>
     </li>
